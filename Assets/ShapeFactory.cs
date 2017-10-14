216e0e00c17accd79b0b0b13f6b98b0767014ce1
jchin <justinchin.bcchs@gmail.com>
2017-10-14 12:06:02 -0600

    first commit

---
+++ Assets/ShapeFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public struct Shape
+{
+    public string name;
+    public DateTime checkInTime;
+    public DateTime checkOutTime;
+    public int buildingId;
+}
+
+public class ShapeFactory : MonoBehaviour
+{ 
+    public static ShapeFactory instance;
+    public Shape rectangle;
+    public Shape circle;
+    public Shape triange;
+    public List<Shape> shapes = new List<Shape>();
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+    }
+    
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void CreateShape(string shapeName)
+    {
+        switch (shapeName)
+        {
+            case "rectangle":
+                return Instantiate<Shape>rectangle;
+                break; 
+            case "triangle":
+                statement(s);
+                break;
+            case "circle":
+                statement(s);
+                break;
+            /* you can have any number of case statements */
+            default: /* Optional */
+                return null;
+        }
+    }
+
+}
