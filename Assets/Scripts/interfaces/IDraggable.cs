65ad0283781c43bf612b6de014b679558d5fad6a
Ian <smithki707@gmail.com>
2017-10-14 12:14:52 -0600

    Initial commit [Ian]

---
+++ Assets/Scripts/interfaces/IDraggable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDraggable {
+
+	bool isDragging { get; set; }
+    void toggleIsDragging();
+}
---
+++ Assets/Scripts/shapes/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Shape :
+    MonoBehaviour,
+    IDraggable,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+
+    public bool isDragging { get; set; }
+
+	public Image imgSrc { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		isDragging = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDragging) {
+            gameObject.transform.position = Input.mousePosition;
+		}
+	}
+
+    public void toggleIsDragging () {
+        isDragging = !isDragging;
+    }
+
+    public void OnPointerDown (PointerEventData eventData) {
+        toggleIsDragging ();
+    }
+
+    public void OnPointerUp (PointerEventData eventData) {
+        toggleIsDragging ();
+    }
+}
